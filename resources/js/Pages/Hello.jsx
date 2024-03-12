2d59a59c8e736a66a45a2b3a359e967e786821e9
Dominik Purgał <amonsir29@gmail.com>
2024-03-12 02:15:08 +0100

    fix js live reload

--- resources/js/Pages/Hello.jsx
+++ resources/js/Pages/Hello.jsx
@@ -4,7 +4,6 @@ import { Head } from '@inertiajs/react';
 export default function Hello() {
     return (
         <>
-
             <Head title="Hello" />
             <div>
                 <h1>Hello</h1>
