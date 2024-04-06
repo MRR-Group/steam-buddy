415f710fb2bbc6f54cfbfb7fd986c51131b55ca7
Amon De Shir <amonsir29@gmail.com>
2024-04-06 02:48:36 +0200

    extend user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property string $description
+ * @property string $image
+ * @property string $password
+ * @property Carbon $email_verified_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class User extends Authenticatable
 {
     use HasFactory;
