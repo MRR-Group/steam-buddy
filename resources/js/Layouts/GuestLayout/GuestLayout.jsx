84ce06d1f57de91e07298a2d310aa54a7ebc3c94
Amon De Shir <amonsir29@gmail.com>
2024-04-06 22:25:16 +0200

    Rewrite GuestLayout

--- resources/js/Layouts/GuestLayout/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout/GuestLayout.jsx
@@ -1,17 +1,10 @@
 import PropTypes from 'prop-types';
-import { ApplicationLogo } from '@/Components/ApplicationLogo/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import './GuestLayout.css';
 
 export const GuestLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-      <div>
-        <Link href="/">
-          <ApplicationLogo className="size-20 fill-current text-gray-500" />
-        </Link>
-      </div>
-
-      <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div className="GuestLayout__background min-h-screen flex flex-col justify-center items-center p-2 sm:p-0">
+      <div className="w-full max-w-md max-[390px]:p-6 bg-transparent-dark backdrop-blur-md shadow-md rounded-2xl flex justify-center items-center">
         {children}
       </div>
     </div>
