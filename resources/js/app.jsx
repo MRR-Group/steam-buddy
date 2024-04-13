1f2dff5889352d3adfc823ec4295a82add9be5a2
Amon De Shir <amonsir29@gmail.com>
2024-04-13 21:54:34 +0200

    fix app name

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -14,7 +14,7 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Steam Buddy';
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
