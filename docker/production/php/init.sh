b6ebd461c84f10317fed1c1df074455c54518052
Amon De Shir <amonsir29@gmail.com>
2024-04-13 04:12:46 +0200

    create storage structure

--- docker/production/php/init.sh
+++ docker/production/php/init.sh
@@ -1,1 +1,11 @@
 chown -R www-data:www-data /app/storage
+mkdir -p  /app/storage
+mkdir -p  /app/storage/public
+
+mkdir -p  /app/storage/framework
+mkdir -p  /app/storage/framework/sessions
+mkdir -p  /app/storage/framework/views
+mkdir -p  /app/storage/framework/cache
+mkdir -p  /app/storage/framework/testing
+
+mkdir -p  /app/storage/logs
