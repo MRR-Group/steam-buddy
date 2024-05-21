73163ef6d7ff0c6c37722ab68b86ccbaf160cb5c
Amon De Shir <amonsir29@gmail.com>
2024-05-21 20:33:59 +0200

    improve Library frontend

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -15,13 +16,37 @@ class LibraryController extends Controller
         /** @var User $user */ 
         $user = $request->user();
         $games = [];
-
-        foreach ($user->games()->get() as $game) {
-            array_push($games, $game->full());
+        $tags = [];
+        
+        foreach ($user->games as $game) {
+            $data = $game->full();
+            $games[] = $data;
+            
+            foreach ($data["tags"] as $tag) {
+                if (!array_key_exists($tag, $tags)) {
+                    $tags[$tag] = ["games" => 1, "name" => $tag];
+                }
+                else {
+                    $tags[$tag]["games"] += 1;
+                }
+            }
         }
+
+        foreach(Tag::MULTIPLAYER_TAGS as $tag) {
+            unset($tags[$tag]);
+        }
+
+        $tags_return = [];
+        
+        foreach($tags as $tag) {
+            $tags_return[] = $tag;
+        }
+        
+        rsort($tags_return);
 
         return Inertia::render("Library", [
             "games" => $games,
+            "tags" => $tags_return,
             "status" => session("status"),
         ]);
     }
