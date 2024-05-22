ce22bec5f05c6e4d8cffd8eacbe54643f8b15077
Amon De Shir <amonsir29@gmail.com>
2024-05-22 04:05:51 +0200

    improve TagButton accessibility

--- resources/js/Components/Tags/TagButton.test.tsx
+++ resources/js/Components/Tags/TagButton.test.tsx
@@ -5,28 +5,23 @@ import { TagButton } from './TagButton';
 
 describe('TagButton', () => {
   it('should render button', () => {
-    render(<TagButton>test</TagButton>);
+    render(<TagButton label='test btn'>test</TagButton>);
 
     expect(screen.getByText('test')).toBeInTheDocument();
   });
 
-  it('should accept button props', () => {
-    render(
-      <div>
-        <TagButton type="submit">test1</TagButton>
-        <TagButton type="submit" active>test2</TagButton>
-      </div>
-    );
+  it('should contains aria role and label', () => {
+    render(<TagButton label='test label' role="button">test</TagButton>);
 
-    expect(screen.getByText('test1').parentElement).toHaveAttribute('type', 'submit');
-    expect(screen.getByText('test2').parentElement).toHaveAttribute('type', 'submit');
-  });
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByLabelText('test label')).toBeInTheDocument();
+  })
 
   it('should add additional css class', () => {
     render(
       <div>
-        <TagButton>test1</TagButton>
-        <TagButton active className="m-5">test2</TagButton>
+        <TagButton label='test btn'>test1</TagButton>
+        <TagButton label='test btn' active className="m-5">test2</TagButton>
       </div>,
     );
 
@@ -37,12 +32,24 @@ describe('TagButton', () => {
   it('should change font color when inactive', () => {
     render(
       <div>
-        <TagButton active>test1</TagButton>
-        <TagButton>test2</TagButton>
+        <TagButton label='test btn' active>test1</TagButton>
+        <TagButton label='test btn'>test2</TagButton>
       </div>,
     );
 
-    expect(screen.getByText('test1')).not.toHaveClass('text-secondary-500');
-    expect(screen.getByText('test2')).toHaveClass('text-secondary-500');
+    expect(screen.getByText('test1')).not.toHaveClass('text-secondary-300');
+    expect(screen.getByText('test2')).toHaveClass('text-secondary-300');
+  });
+
+  it('should set aria-checked to true when active', () => {
+    render(
+      <div>
+        <TagButton label='test btn' active>test1</TagButton>
+        <TagButton label='test btn'>test2</TagButton>
+      </div>,
+    );
+
+    expect(screen.getByText('test1')).toHaveAttribute('aria-checked', "true");
+    expect(screen.getByText('test2')).toHaveAttribute('aria-checked', "false");
   });
 });
--- resources/js/Components/Tags/TagButton.tsx
+++ resources/js/Components/Tags/TagButton.tsx
@@ -1,21 +1,31 @@
-import { ButtonHTMLAttributes } from 'react';
+import { HTMLAttributes } from "react";
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-  active?: boolean
+type Props = HTMLAttributes<HTMLDivElement> & {
+  active?: boolean,
+  className?: string,
+  label: string,
+  role?: "button" | "checkbox"
 };
 
 export const TagButton = ({
   className = '',
   active,
   children,
+  role = "button",
+  label,
   ...props
 }: Props) => (
-  <button
+  <div
     {...props}
     className={`m-1 p-px block text-text text-sm font-bold bg-gradient-to-r min-w-16 rounded-xl from-gradient-light via-gradient-medium to-gradient-dark ${className}`}
   >
-    <div className={`pl-4 pr-4 pt-2 pb-2 w-full h-full whitespace-nowrap transition-colors rounded-xl ${!active && 'bg-dark text-secondary-500'}`}>
+    <div
+      className={`pl-4 pr-4 pt-2 pb-2 w-full h-full whitespace-nowrap transition-colors rounded-xl ${!active && 'bg-dark text-secondary-300'}`} 
+      aria-checked={active ?? false}
+      role={role}
+      aria-label={label}
+    >
       {children}
     </div>
-  </button>
+  </div>
 );
