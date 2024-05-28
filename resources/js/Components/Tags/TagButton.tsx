8800a0a2f54b689853fcd1356df0b7f884b16323
Amon De Shir <amonsir29@gmail.com>
2024-05-28 19:30:30 +0200

    change tag button cursor

--- resources/js/Components/Tags/TagButton.tsx
+++ resources/js/Components/Tags/TagButton.tsx
@@ -17,7 +17,7 @@ export const TagButton = ({
 }: Props) => (
   <div
     {...props}
-    className={`m-1 p-px block text-text text-sm font-bold bg-gradient-to-r min-w-16 rounded-xl from-gradient-light via-gradient-medium to-gradient-dark ${className}`}
+    className={`m-1 p-px block text-text text-sm font-bold bg-gradient-to-r min-w-16 rounded-xl from-gradient-light via-gradient-medium to-gradient-dark cursor-pointer ${className}`}
   >
     <div
       className={`pl-4 pr-4 pt-2 pb-2 w-full h-full whitespace-nowrap transition-colors rounded-xl ${!active && 'bg-dark text-secondary-300'}`} 
