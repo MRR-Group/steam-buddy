9b43663523e0a43ce55bd1a84a391d325ea31cda
Amon De Shir <amonsir29@gmail.com>
2024-05-29 03:22:07 +0200

    use game component instead of image

--- resources/js/Pages/Game/Show.tsx
+++ resources/js/Pages/Game/Show.tsx
@@ -3,6 +3,11 @@ import { AuthenticatedLayout } from '../../Layouts/AuthenticatedLayout/Authentic
 import { useMemo } from 'react';
 import { Tags } from '../../Components/Tags/Tags';
 import { PrimaryButton } from '@/Components/PrimaryButton/PrimaryButton';
+import { Game } from '../../Components/Game/Game';
+
+// className='xl:h-auto lg:w-full lg:h-auto sm:w-6/12 xs:w-8/12 w-full h-auto'
+// blurredImgClassName='w-full h-full md:min-h-96 max-h-full'
+// imgClassName='max-h-full'
 
 export type ShowPageProps = {
   user_id: number;
@@ -31,7 +36,13 @@ export const Show = ({ user_id, user_name, user_email, game_id, game_name, game_
       if (node.nodeType === Node.TEXT_NODE) {
         text += (node as TextNode).data;
       }
+
+      else if (node.nodeName === "BR") {
+        text += "\n";
+      }
     }
+
+    text = text.trim().replaceAll("\n\n", "\n");
 
     return text;
   };
@@ -63,32 +74,32 @@ export const Show = ({ user_id, user_name, user_email, game_id, game_name, game_
       <Head title={game_name} />
 
       <h1 className='text-text text-4xl w-full text-center mb-8'>{game_name}</h1>
-      <div className='w-full flex flex-col md:flex-row'>
-        <div className='flex justify-center mb-2'>
-          <img
-            src={game_cover}
-            className='w-10/12 rounded-xl'
-            alt={`Cover image of the game "${game_name}"`}
+      <Tags
+        selected={game_tags}
+        tags={tags}
+        tagRole='button'
+        itemAriaLabel={({name}) => `Search games with ${name} tag`}
+        onUnselect={showGamesWithTag}
+      />
+
+
+      <div className='w-full flex flex-col items-center lg:items-stretch lg:flex-row lg:justify-between lg:h-[calc(100%-13rem)] lg:min-h-[750px]'>
+        <div className='flex items-center justify-center w-full xs:w-3/4  sm:w-1/2 md:w-1/2 lg:w-full h-full max-w-lg pr-5 pl-5 lg:pl-1'>
+          <Game
+            game_cover={game_cover}
+            game_name={game_name}
           />
         </div>
 
-        <div className='md:w-2/4 flex flex-col justify-between items-center'>
-          <Tags
-            selected={game_tags}
-            tags={tags}
-            tagRole='button'
-            itemAriaLabel={({name}) => `Search games with ${name} tag`}
-            onUnselect={showGamesWithTag}
-          />
-
-          <p className='text-justify text-text font-inter p-2'>
+        <div className='lg:w-2/4 flex flex-col justify-between items-center'>
+          <p className=' text-text font-inter p-2 w-full whitespace-pre-line h-full flex justify-center items-center'>
             {text}
           </p>
 
           {
             is_owner && (
               <Link method='post' href={route('match.find', {game_id})} className="w-full flex justify-center">
-                <div className='p-2 pt-0 w-full md:max-w-80'>
+                <div className='p-5 pt-3 w-full md:max-w-80'>
                   <PrimaryButton type='submit'>Find Mate</PrimaryButton>
                 </div>
               </Link>
