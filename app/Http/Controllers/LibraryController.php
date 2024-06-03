42c5b483757e5c04d42caa8fda9cc8eae3ea31b2
Amon De Shir <amonsir29@gmail.com>
2024-06-03 09:09:02 +0200

    sort games by name

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -8,13 +8,33 @@ use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class LibraryController extends Controller
 {
-    public function show(Request $request)
+    public function show(Request $request): Response
     {
         /** @var User $user */ 
         $user = $request->user();
+        $selected_tags = $request->query("tags");
+        
+        if ($selected_tags === null) {
+            $selected_tags = [];
+        } else if (!is_array($selected_tags)) {
+            $selected_tags = [$selected_tags];
+        }
+        
+        ["games" => $games, "tags" => $tags] = $this->get_games($user);
+
+        return Inertia::render("Library", [
+            "games" => $games,
+            "tags" => $tags,
+            "default_selected_tags" => $selected_tags,
+            "status" => session("status"),
+        ]);
+    }
+
+    protected function get_games(User $user): array {
         $games = [];
         $tags = [];
 
@@ -24,38 +44,31 @@ class LibraryController extends Controller
 
             foreach ($data["tags"] as $tag) {
                 if (!array_key_exists($tag, $tags)) {
-                    $tags[$tag] = ["games" => 1, "name" => $tag];
+                    $tags[$tag] = ["name" => $tag, "games" => 1];
                 } else {
                     ++$tags[$tag]["games"];
                 }
             }
         }
 
+        return [
+            "games" => $games,
+            "tags" => $this->sort_tags($tags)
+        ];
+    }
+
+    protected function sort_tags(array $tags): array {
         foreach (Tag::MULTIPLAYER_TAGS as $tag) {
             unset($tags[$tag]);
         }
 
-        $tags_return = [];
+        sort($tags);
+        $result = [];
 
         foreach ($tags as $tag) {
-            $tags_return[] = $tag;
+            $result[] = $tag;
         }
 
-        rsort($tags_return);
-
-        $selected_tags = $request->query("tags");
-
-        if ($selected_tags === null) {
-            $selected_tags = [];
-        } else if (!is_array($selected_tags)) {
-            $selected_tags = [$selected_tags];
-        }
-
-        return Inertia::render("Library", [
-            "games" => $games,
-            "tags" => $tags_return,
-            "default_selected_tags" => $selected_tags,
-            "status" => session("status"),
-        ]);
+        return $result;
     }
 }
