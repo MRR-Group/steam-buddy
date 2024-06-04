285f1e1841cfb4c176c37a08b26c59b675efae3c
Amon De Shir <amonsir29@gmail.com>
2024-06-04 02:18:15 +0200

    add i18n for library page

--- resources/js/Pages/Library.tsx
+++ resources/js/Pages/Library.tsx
@@ -4,6 +4,7 @@ import { Tags } from '../Components/Tags/Tags';
 import { useMemo, useState } from 'react';
 import { TextInput } from '../Components/TextInput/TextInput';
 import { GameLink } from '../Components/GameLink/GameLink';
+import { useTranslate } from '../Hooks/Translate/Translate';
 
 type Props = {
   auth: {
@@ -32,7 +33,8 @@ export const Library = ({ auth, games, tags, default_selected_tags }: Props) => 
   
   const selectTag = (tag: string) => setSelectedTags(selected => [...selected, tag]);
   const unselectTag = (tag: string) => setSelectedTags(selected => selected.filter(name => name !== tag));
-  
+  const { t } = useTranslate("app");
+
   const filteredGames = useMemo(() => games
     .filter((game) => selectedTags.length === 0 || selectedTags.every(tag => game.tags.includes(tag)))
     .filter((game) => searchText.length === 0 || game.name.toLowerCase().includes(searchText.trim().toLowerCase())),
@@ -41,9 +43,9 @@ export const Library = ({ auth, games, tags, default_selected_tags }: Props) => 
   return (
     <AuthenticatedLayout
       user={auth.user}
-      title="Library"
+      title={t("Library")}
     >
-      <Head title="Library" />
+      <Head title={t("Library")} />
 
       <div className="w-full flex justify-center mb-10">
         <div className="w-full 2xl:w-6/12 xl:w-7/12 lg:w-8/12 md:w-10/12 flex items-stretch flex-col media">
@@ -58,7 +60,7 @@ export const Library = ({ auth, games, tags, default_selected_tags }: Props) => 
           
           <TextInput 
             name="Search"
-            label='Search game' 
+            label={t("Search game")}
             className='mt-5' 
             value={searchText} 
             onInput={(e) => setSearchText(e.currentTarget.value)}
